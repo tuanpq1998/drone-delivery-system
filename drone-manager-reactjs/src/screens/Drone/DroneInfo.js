16467c09ff40de7bf5725ae29471f5c3f884ce2d
tuanpq1998 <tuanquang123321@gmail.com>
2022-12-04 17:43:52 +0700

    add front-end extra drone info

--- drone-manager-reactjs/src/screens/Drone/DroneInfo.js
+++ drone-manager-reactjs/src/screens/Drone/DroneInfo.js
@@ -16,7 +16,17 @@ export default class DroneInfo extends Component {
         <div className="card-body">
             <dl className="row">
               <dt className="col-md-2">Altitude</dt>
-              <dd className="col-md-4">{drone.locationAltitude}</dd>
+              <dd className="col-md-2">{drone.locationAltitude}</dd>
+
+              <dt className="col-md-2">Velocity vertical</dt>
+              <dd className="col-md-2">{drone.velocityVertical} m/s</dd>
+
+              <dt className="col-md-2">Velocity horizontal</dt>
+              <dd className="col-md-2">{drone.velocityHorizontal} m/s</dd>
+
+              <dt className="col-md-2">Battery</dt>
+              <dd className="col-md-4">{drone.batteryPercent*100}% - {drone.batteryVoltage}V</dd>
+
               <dt className="col-md-2">Last updated: </dt>
               <dd className="col-md-4">{drone.lastUpdatedAt}</dd>
               { drone.activeMission ? (
@@ -24,7 +34,7 @@ export default class DroneInfo extends Component {
                 <dt className="col-md-2">Status: </dt>
                 <dd className="col-md-10">{drone.activeMission.status}</dd>
                 <dt className="col-md-2">On mission: </dt>
-                <dd className="col-md-10">#{drone.activeMission.missionIdentifier}</dd>
+                <dd className="col-md-10"><code>#{drone.activeMission.missionIdentifier}</code></dd>
                 <dt className="col-md-2">Logs: </dt>
                 <dd className="col-md-10" style={{backgroundColor: "#faebd7"}}><pre><code>
                 {drone.activeMission.logs == null ? "" : drone.activeMission.logs.split("|").filter(l => l.trim() !== "").reverse().join("\n")}
